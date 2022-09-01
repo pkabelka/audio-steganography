530a6e3805f7e88657f1a8b5ccb75afa079a10ea
pkabelka <pkabelka@email.cz>
2022-09-01 16:57:38 +0200

    bruteforce working delays

--- audio-steganography.py
+++ audio-steganography.py
@@ -1,16 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile
+import scipy.signal
 import sys
 # import os
 
 # np.set_printoptions(threshold=sys.maxsize)
 
-encode = np.array([0,0,0,0,0,0,0,0])
-encode = np.array([0,1,0,1,1,0,1,1])
-# hide = [1,1,1,1,1,1,1,1]
-# hide = np.array([0,1,0,0,1,0,0,0,0,1,1,0,0,1,0,1,0,1,1,0,1,1,0,0,0,1,1,0,1,1,0,0,0,1,1,0,1,1,1,1,0,0,1,0,0,0,0,0,0,1,1,1,0,1,1,1,0,1,1,0,1,1,1,1,0,1,1,1,0,0,1,0,0,1,1,0,1,1,0,0,0,1,1,0,0,1,0,0,0,0,1,0,0,0,0,1])
-message = 'H'
+message = 'msg'
 encode = []
 for c in message:
     for b in '{0:08b}'.format(ord(c), 'b'):
@@ -38,35 +35,49 @@ mixer = np.hstack(mixer)
 print(mixer)
 print('Mixer len:', len(mixer))
 
-d0 = 105
-h0 = np.append(np.zeros(d0), [1])
+delay_pairs = []
+end = False
+for d0 in range(250, 350):
+    h0 = np.append(np.zeros(d0), [1])
+    for d1 in range(d0, d0+100):
 
-d1 = 110
-h1 = np.append(np.zeros(d1), [1])
+        # d0 = 105
 
-k0 = np.convolve(h0, s)
-k1 = np.convolve(h1, s)
+        # d1 = 110
+        h1 = np.append(np.zeros(d1), [1])
 
-sp = np.pad(np.array(s), (0, len(k1)-len(s)))
-x = np.zeros(len(sp))
-x = sp[:len(mixer)]+k1[:len(mixer)] * mixer + sp[:len(mixer)]+k0[:len(mixer)] * (1-mixer)
+        k0 = scipy.signal.fftconvolve(h0, s)
+        k1 = scipy.signal.fftconvolve(h1, s)
 
-x_f = x - np.mean(x)
-x_f = x_f / np.abs(x_f).max()
-scipy.io.wavfile.write('echo.wav', sr, x_f)
+        sp = np.pad(np.array(s), (0, len(k1)-len(s)))
+        x = np.zeros(len(sp))
+        x = sp[:len(mixer)]+k1[:len(mixer)] * mixer + sp[:len(mixer)]+k0[:len(mixer)] * (1-mixer)
 
-decoded = []
-split = seg_split(x_f, c+1)[:-1]
-for seg in split:
-    cn = np.fft.ifft(np.log(np.abs(np.fft.fft(seg))))
-    if cn[d0+1] > cn[d1+1]:
-        decoded.append(0)
-    else:
-        decoded.append(1)
+        x_f = x - np.mean(x)
+        x_f = x_f / np.abs(x_f).max()
+        # scipy.io.wavfile.write('echo.wav', sr, x_f)
 
-decoded = np.array(decoded)
-print(decoded)
-decoded_message = ''
-for i in range(0, len(decoded), 7):
-    decoded_message += chr(int(''.join([str(x) for x in decoded[i:i+7]]), 2))
-print(decoded_message)
+        decoded = []
+        split = seg_split(x_f, c+1)[:-1]
+        for seg in split:
+            cn = np.fft.ifft(np.log(np.abs(np.fft.fft(seg))))
+            if cn[d0+1] > cn[d1+1]:
+                decoded.append(0)
+            else:
+                decoded.append(1)
+
+        decoded = np.array(decoded)
+        print(decoded)
+        decoded_message = ''
+        for i in range(0, len(decoded), 8):
+            decoded_message += chr(int(''.join([str(x) for x in decoded[i:i+8]]), 2))
+        print(decoded_message)
+
+        if np.all(decoded == encode):
+            delay_pairs.append((d0, d1))
+            end = True
+            break
+    if end:
+        break
+
+print(delay_pairs)
