1b14db2aad14e87409c84de74792a69a80b6c97e
pkabelka <pkabelka@email.cz>
2022-10-10 20:35:41 +0200

    stat_utils: added mse, rmsd, psnr_db

--- audio_steganography/stat_utils.py
+++ audio_steganography/stat_utils.py
@@ -32,6 +32,74 @@ def snr_db(x: np.ndarray | List, y: np.ndarray | List) -> float:
         raise
     return 10 * np.log10((np.sum(x**2)) / (np.sum((x - y)**2)))
 
+def mse(x: np.ndarray | List, y: np.ndarray | List) -> float:
+    """Calculates the mean square error of clean signal and noisy signal.
+
+    Parameters
+    ----------
+    x : np.ndarray | List
+        Clean signal array.
+    y : np.ndarray | List
+        Noisy signal array.
+
+    Returns
+    -------
+    mse : float
+        Mean square error value.
+    """
+    x = np.asanyarray(x)
+    y = np.asanyarray(y)
+    n = len(x)
+    if n != len(y):
+        raise
+    return 1/n * np.sum((x - y)**2)
+
+def rmsd(x: np.ndarray | List, y: np.ndarray | List) -> float:
+    """Calculates the root mean square deviation of clean signal and noisy
+    signal.
+
+    Parameters
+    ----------
+    x : np.ndarray | List
+        Clean signal array.
+    y : np.ndarray | List
+        Noisy signal array.
+
+    Returns
+    -------
+    rmsd : float
+        Root mean square deviation value.
+    """
+    x = np.asanyarray(x)
+    y = np.asanyarray(y)
+    n = len(x)
+    if n != len(y):
+        raise
+    return np.sqrt(mse(x, y))
+
+def psnr_db(x: np.ndarray | List, y: np.ndarray | List) -> float:
+    """Calculates the peak signal-to-noise ratio of clean signal and noisy
+    signal in decibells.
+
+    Parameters
+    ----------
+    x : np.ndarray | List
+        Clean signal array.
+    y : np.ndarray | List
+        Noisy signal array.
+
+    Returns
+    -------
+    psnr : float
+        Peak signal-to-noise ratio value in decibells.
+    """
+    x = np.asanyarray(x)
+    y = np.asanyarray(y)
+    n = len(x)
+    if n != len(y):
+        raise
+    return 10 * np.log10(1.0 / mse(x, y))
+
 def ber_percent(
         x: np.ndarray | List[List],
         y: np.ndarray | List[List],
