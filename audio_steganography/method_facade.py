fixed LSB method by not normalizing input source

audio_steganography/method_facade.py
@@ -118,10 +118,11 @@ class MethodFacade:
         self.source_sr: int = self.source_sr
 
         # normalize to float64 [-1; 1]
-        self.source_data: np.ndarray[
-            typing.Any,
-            np.dtype[np.float64]] = (self.source_data /
-                np.abs(self.source_data).max()).astype(np.float64)
+        # FIXME: this breaks LSB method
+        # self.source_data: np.ndarray[
+        #     typing.Any,
+        #     np.dtype[np.float64]] = (self.source_data /
+        #         np.abs(self.source_data).max()).astype(np.float64)
 
         if self.mode == Mode.encode:
 