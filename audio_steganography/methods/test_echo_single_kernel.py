87fde49efbfa4a76d3557f5b15fa9083b95934f4
pkabelka <pkabelka@email.cz>
2022-11-25 01:57:26 +0100

    echo: added tests

---
+++ audio_steganography/methods/test_echo_single_kernel.py
@@ -0,0 +1,80 @@
+import unittest
+import numpy as np
+from ..test import *
+from .echo_single_kernel import Echo_single_kernel
+from ..exceptions import SecretSizeTooLarge
+from ..stat_utils import ber_percent
+
+class TestLSB(unittest.TestCase):
+    def setUp(self) -> None:
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def testWrongTypeSecret(self):
+        self.assertRaisesRegex(
+            TypeError,
+            'secret_data must be of type numpy.uint8',
+            Echo_single_kernel,
+            source_empty,
+            secret_empty)
+
+        self.assertRaisesRegex(
+            TypeError,
+            'secret_data must be of type numpy.uint8',
+            Echo_single_kernel,
+            source_empty,
+            secret_int16_empty)
+
+        self.assertRaisesRegex(
+            TypeError,
+            'secret_data must be of type numpy.uint8',
+            Echo_single_kernel,
+            source_empty,
+            secret_uint16_42)
+
+    def testEmptySecretEncode(self):
+        echo = Echo_single_kernel(source_uint8_len_32, secret_uint8_empty)
+        output, additional_output = echo.encode()
+
+        self.assertEqual(output.size, source_uint8_len_32.size)
+        np.testing.assert_equal(output, source_uint8_len_32)
+        self.assertEqual(additional_output['l'], secret_uint8_empty.size)
+
+    def testEmptyEncode(self):
+        echo = Echo_single_kernel(source_int16_empty, secret_uint8_empty)
+        output, additional_output = echo.encode()
+
+        self.assertEqual(output.size, source_int16_empty.size)
+        np.testing.assert_equal(output, source_int16_empty)
+        self.assertEqual(additional_output['l'], secret_uint8_empty.size)
+
+    def testEmptySourceEncode(self):
+        echo = Echo_single_kernel(source_uint8_empty, secret_uint8_42)
+        self.assertRaises(SecretSizeTooLarge, echo.encode)
+
+        echo = Echo_single_kernel(source_int16_empty, secret_uint8_42)
+        self.assertRaises(SecretSizeTooLarge, echo.encode)
+
+        echo = Echo_single_kernel(source_int32_empty, secret_uint8_42)
+        self.assertRaises(SecretSizeTooLarge, echo.encode)
+
+        echo = Echo_single_kernel(source_float16_empty, secret_uint8_42)
+        self.assertRaises(SecretSizeTooLarge, echo.encode)
+
+        echo = Echo_single_kernel(source_float32_empty, secret_uint8_42)
+        self.assertRaises(SecretSizeTooLarge, echo.encode)
+
+        echo = Echo_single_kernel(source_float64_empty, secret_uint8_42)
+        self.assertRaises(SecretSizeTooLarge, echo.encode)
+
+    def testEncodeDecode42(self):
+        echo = Echo_single_kernel(source_int16_len_131072, secret_uint8_42)
+        output, additional_output = echo.encode(d0=2306, d1=3000)
+
+        self.assertEqual(additional_output['l'], secret_uint8_42.size)
+
+        echo = Echo_single_kernel(output)
+        output, additional_output = echo.decode(d0=250, d1=350, l=additional_output['l'])
+        self.assertEqual(ber_percent(secret_uint8_42, output), 31.25)
--- audio_steganography/test/__init__.py
+++ audio_steganography/test/__init__.py
@@ -33,6 +33,46 @@ size = 32
 source_uint8_len_32 = np.array(np.random.randint(256, size=size)).astype(np.uint8)
 source_int16_len_32 = np.array(np.random.randint(65536, size=size)).astype(np.int16)
 source_int32_len_32 = np.array(np.random.randint(2147483648, size=size)).astype(np.int32)
-source_float16_len_32 = np.array(np.random.rand(32) * 2 - 1).astype(np.float16)
-source_float32_len_32 = np.array(np.random.rand(32) * 2 - 1).astype(np.float32)
-source_float64_len_32 = np.array(np.random.rand(32) * 2 - 1).astype(np.float64)
+source_float16_len_32 = np.array(np.random.rand(size) * 2 - 1).astype(np.float16)
+source_float32_len_32 = np.array(np.random.rand(size) * 2 - 1).astype(np.float32)
+source_float64_len_32 = np.array(np.random.rand(size) * 2 - 1).astype(np.float64)
+
+### length 1024 seed=42
+size = 1024
+rs = np.random.RandomState(seed=42)
+source_uint8_len_1024 = np.array(rs.randint(256, size=size)).astype(np.uint8)
+source_int16_len_1024 = np.array(rs.randint(65536, size=size)).astype(np.int16)
+source_int1024_len_1024 = np.array(rs.randint(2147483648, size=size)).astype(np.int32)
+source_float16_len_1024 = np.array(rs.rand(size) * 2 - 1).astype(np.float16)
+source_float32_len_1024 = np.array(rs.rand(size) * 2 - 1).astype(np.float32)
+source_float64_len_1024 = np.array(rs.rand(size) * 2 - 1).astype(np.float64)
+
+### length 32768 seed=42
+size = 32768
+rs = np.random.RandomState(seed=42)
+source_uint8_len_32768 = np.array(rs.randint(256, size=size)).astype(np.uint8)
+source_int16_len_32768 = np.array(rs.randint(65536, size=size)).astype(np.int16)
+source_int32_len_32768 = np.array(rs.randint(2147483648, size=size)).astype(np.int32)
+source_float16_len_32768 = np.array(rs.rand(size) * 2 - 1).astype(np.float16)
+source_float32_len_32768 = np.array(rs.rand(size) * 2 - 1).astype(np.float32)
+source_float64_len_32768 = np.array(rs.rand(size) * 2 - 1).astype(np.float64)
+
+### length 131072 seed=42
+size = 131072
+rs = np.random.RandomState(seed=42)
+source_uint8_len_131072 = np.array(rs.randint(256, size=size)).astype(np.uint8)
+source_int16_len_131072 = np.array(rs.randint(65536, size=size)).astype(np.int16)
+source_int32_len_131072 = np.array(rs.randint(2147483648, size=size)).astype(np.int32)
+source_float16_len_131072 = np.array(rs.rand(size) * 2 - 1).astype(np.float16)
+source_float32_len_131072 = np.array(rs.rand(size) * 2 - 1).astype(np.float32)
+source_float64_len_131072 = np.array(rs.rand(size) * 2 - 1).astype(np.float64)
+
+### length 524288 seed=42
+# size = 524288
+# rs = np.random.RandomState(seed=42)
+# source_uint8_len_524288 = np.array(rs.randint(256, size=size)).astype(np.uint8)
+# source_int16_len_524288 = np.array(rs.randint(65536, size=size)).astype(np.int16)
+# source_int32_len_524288 = np.array(rs.randint(2147483648, size=size)).astype(np.int32)
+# source_float16_len_524288 = np.array(rs.rand(size) * 2 - 1).astype(np.float16)
+# source_float32_len_524288 = np.array(rs.rand(size) * 2 - 1).astype(np.float32)
+# source_float64_len_524288 = np.array(rs.rand(size) * 2 - 1).astype(np.float64)
