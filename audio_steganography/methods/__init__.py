4eb66a25fba78d39766d92aded74ea1f5aaab84f
pkabelka <pkabelka@email.cz>
2023-01-23 23:16:37 +0100

    added bipolar backward forward echo method

--- audio_steganography/methods/__init__.py
+++ audio_steganography/methods/__init__.py
@@ -11,6 +11,7 @@ from enum import Enum as _Enum
 from .echo_single import EchoSingle
 from .echo_bipolar import EchoBipolar
 from .echo_bf import EchoBF
+from .echo_bipolar_bf import EchoBipolarBF
 from .lsb import LSB
 from .phase_coding import PhaseCoding
 from .dsss import DSSS
@@ -21,6 +22,7 @@ class MethodEnum(_Enum):
     echo_single = EchoSingle
     echo_bipolar = EchoBipolar
     echo_bf = EchoBF
+    echo_bipolar_bf = EchoBipolarBF
     lsb = LSB
     phase = PhaseCoding
     dsss = DSSS
