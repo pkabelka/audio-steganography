e597986f68bdb83594166af4cb5920cd3629d22f
pkabelka <pkabelka@email.cz>
2023-01-26 17:46:20 +0100

    stat_utils: moved snr_db above psnr_db

--- audio_steganography/stat_utils.py
+++ audio_steganography/stat_utils.py
@@ -9,41 +9,6 @@
 import numpy as np
 from typing import List, Union
 
-def snr_db(x: Union[np.ndarray, List], y: Union[np.ndarray, List]) -> float:
-    """Calculates the decibell signal-to-noise ratio of clean signal and noisy
-    signal.
-
-    Parameters
-    ----------
-    x : np.ndarray | List
-        Clean signal array.
-    y : np.ndarray | List
-        Noisy signal array.
-
-    Returns
-    -------
-    snr : float
-        Signal-to-noise ratio value in decibells.
-    """
-    x = np.asanyarray(x)
-    y = np.asanyarray(y)
-
-    n = min(x.size, y.size)
-    x = x[:n]
-    y = y[:n]
-
-    x = x - np.mean(x)
-    if np.abs(x).max() != 0:
-        x = x / np.abs(x).max()
-    x = x.astype(np.float64)
-
-    y = y - np.mean(y)
-    if np.abs(y).max() != 0:
-        y = y / np.abs(y).max()
-    y = y.astype(np.float64)
-
-    return 10 * np.log10((np.sum(x**2)) / (np.sum((x - y)**2)))
-
 def mse(x: Union[np.ndarray, List], y: Union[np.ndarray, List]) -> float:
     """Calculates the mean square error of clean signal and noisy signal.
 
@@ -101,6 +66,41 @@ def rmsd(x: Union[np.ndarray, List], y: Union[np.ndarray, List]) -> float:
     x = x[:n]
     y = y[:n]
     return np.sqrt(mse(x, y))
+
+def snr_db(x: Union[np.ndarray, List], y: Union[np.ndarray, List]) -> float:
+    """Calculates the decibell signal-to-noise ratio of clean signal and noisy
+    signal.
+
+    Parameters
+    ----------
+    x : np.ndarray | List
+        Clean signal array.
+    y : np.ndarray | List
+        Noisy signal array.
+
+    Returns
+    -------
+    snr : float
+        Signal-to-noise ratio value in decibells.
+    """
+    x = np.asanyarray(x)
+    y = np.asanyarray(y)
+
+    n = min(x.size, y.size)
+    x = x[:n]
+    y = y[:n]
+
+    x = x - np.mean(x)
+    if np.abs(x).max() != 0:
+        x = x / np.abs(x).max()
+    x = x.astype(np.float64)
+
+    y = y - np.mean(y)
+    if np.abs(y).max() != 0:
+        y = y / np.abs(y).max()
+    y = y.astype(np.float64)
+
+    return 10 * np.log10((np.sum(x**2)) / (np.sum((x - y)**2)))
 
 def psnr_db(x: Union[np.ndarray, List], y: Union[np.ndarray, List]) -> float:
     """Calculates the peak signal-to-noise ratio of clean signal and noisy
