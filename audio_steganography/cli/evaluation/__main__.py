1eca711d8b060b7c0175ce6a0d54efa54e1656d7
pkabelka <pkabelka@email.cz>
2023-03-22 20:26:36 +0100

    eval: use multiprocessing to parallelize evaluation

--- audio_steganography/cli/evaluation/__main__.py
+++ audio_steganography/cli/evaluation/__main__.py
@@ -17,8 +17,46 @@ from typing import List
 from pathlib import Path
 import scipy.io.wavfile
 import logging
-import pandas as pd
 import time
+import multiprocessing as mp
+
+def evaluation_process(
+        dataset,
+        category,
+        file,
+        methods,
+        columns,
+        extended,
+        output_dir,
+    ):
+    logging.info(f'file: {file}')
+    for method in methods:
+        # Read source WAV data
+        try:
+            source_sr, source_data = scipy.io.wavfile.read(file)
+        except FileNotFoundError:
+            error_exit('source file not found', ExitCode.FileNotFound)
+        except ValueError as e:
+            error_exit(str(e), ExitCode.WavReadError)
+
+        method_res = evaluate_method(
+            method,
+            source_data,
+            source_sr,
+            columns,
+            extended,
+        )
+        method_res['dataset'] = dataset.name
+        method_res['category'] = category.name
+        method_res['file'] = file.name
+
+        output_path = output_dir / f'{dataset.name}' / f'{category.name}'
+        output_path.mkdir(parents=True, exist_ok=True)
+
+        method_res.to_csv(
+            output_path / f'{file.name}.csv',
+            sep=';',
+        )
 
 def main():
     """The main function of the evaluation program.
@@ -31,11 +69,7 @@ def main():
     if not args.log:
         logging.disable(logging.INFO)
 
-    # check the output file
-    output_file = Path(args.output)
-    if args.output != '-':
-        if output_file.exists() and not args.overwrite:
-            error_exit('output file already exists', ExitCode.OutputFileExists)
+    output_dir = Path(args.output)
 
     # get the requested methods from args
     methods: List[MethodEnum] = [method for method in MethodEnum]
@@ -64,7 +98,6 @@ def main():
         'time_to_encode',
         'time_to_decode',
     ]
-    stats = pd.DataFrame( columns=columns)
 
     # evaluate chosen methods on dataset files
     dataset_root = Path(args.workdir)
@@ -73,6 +106,9 @@ def main():
         x for x in dataset_root.iterdir()
         if x.is_dir() and not x.name.startswith('.')
     ]
+
+    # multiprocess pool
+    pool = mp.Pool(mp.cpu_count())
 
     start_time = time.perf_counter()
     for dataset in datasets:
@@ -87,45 +123,36 @@ def main():
                 x.is_file() and x.suffix.lower() == '.wav']
             logging.debug(files)
             for file in files:
-                logging.info(f'file: {file}')
-                for method in methods:
-                    # Read source WAV data
-                    try:
-                        source_sr, source_data = scipy.io.wavfile.read(file)
-                    except FileNotFoundError:
-                        error_exit('source file not found', ExitCode.FileNotFound)
-                    except ValueError as e:
-                        error_exit(str(e), ExitCode.WavReadError)
-
-                    method_res = evaluate_method(
-                        method,
-                        source_data,
-                        source_sr,
+                # add files to pool
+                pool.apply_async(
+                    evaluation_process,
+                    args=(
+                        dataset,
+                        category,
+                        file,
+                        methods,
                         columns,
                         args.extended,
-                    )
-                    method_res['dataset'] = dataset.name
-                    method_res['category'] = category.name
-                    method_res['file'] = file.name
-                    stats = pd.concat([stats, method_res], ignore_index=True)
+                        output_dir,
+                    ),
+                )
+
+    # run processes
+    pool.close()
+    pool.join()
 
     logging.info(f'evaluation took: {time.perf_counter() - start_time}')
 
-    stats = stats.sort_values(
-        [
-            'method',
-            'dataset',
-            'category',
-            'file',
-            'ber_percent',
-            'snr_db',
-        ],
-    )
-    # output stats to STDOUT or CSV file
-    if args.output == '-':
-        print(stats)
-    else:
-        stats.to_csv(output_file, sep=';')
+    # stats = stats.sort_values(
+    #     [
+    #         'method',
+    #         'dataset',
+    #         'category',
+    #         'file',
+    #         'ber_percent',
+    #         'snr_db',
+    #     ],
+    # )
 
 if __name__ == '__main__':
     main()
