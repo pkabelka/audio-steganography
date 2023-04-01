e88ad10bf59f42532c1ee6d539d2fa8182d4c972
pkabelka <pkabelka@email.cz>
2023-04-02 00:41:55 +0200

    eval: WIP: added process.py to filter the evaluation data

---
+++ audio_steganography/cli/evaluation/process.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# File: process.py
+# Author: Petr Kabelka <xkabel09 at stud.fit.vutbr.cz>
+
+"""This file contains the main() function for the program to process audio
+steganography methods evaluation results.
+"""
+
+import argparse
+from typing import Tuple, Any
+from pathlib import Path
+import pandas as pd
+
+def parse_args() -> Tuple[Any, argparse.ArgumentParser]:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-d',
+        '--datasets', metavar='PATH',
+        action='store',
+        help='path to a directory containing dataset evaluation results; '+
+            'expected structure is: <dataset root>/<datasets>/<categories>/<files>; '+
+            'directories starting with "." will be ignored',
+        required=True)
+
+    parser.add_argument(
+        '-o',
+        '--output',
+        metavar='OUTPUT_DIR',
+        action='store',
+        help='path to an output directory; EXISTING FILES WILL BE OVERWRITTEN',
+        default=None)
+
+    # Parse all args
+    args = parser.parse_args()
+    return args, parser
+
+def main():
+    """The main function of the evaluation data processing program.
+    """
+    args, _ = parse_args()
+    output_dir = Path(args.output)
+
+    file_dfs = []
+
+    dataset_root = Path(args.datasets)
+    datasets = [
+        x for x in dataset_root.iterdir()
+        if x.is_dir() and not x.name.startswith('.')
+    ]
+    for dataset in datasets:
+        categories = [
+            x for x in dataset.iterdir()
+            if x.is_dir() and not x.name.startswith('.')
+        ]
+        for category in categories:
+            files = [
+                x for x in category.iterdir() if
+                x.is_file() and not x.name.startswith('.')
+                            and x.suffix.lower() == '.csv'
+            ]
+            for file in files:
+                file_dfs.append(pd.read_csv(file, sep=';'))
+
+    df_all = pd.concat(file_dfs, ignore_index=True)
+
+if __name__ == '__main__':
+    main()
